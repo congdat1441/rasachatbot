e68acc2a7cf03f72a94ed77b84bebd87f016b012
Cong Dat <datlionking@outlook.com.vn>
2021-12-17 20:36:42 +0700

    rasa

---
+++ actions/actions.py
@@ -0,0 +1,27 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+import re
+from typing import Any, Dict, List, Text
+from rasa.nlu.tokenizers.tokenizer import Token, Tokenizer
+from rasa.nlu.training_data import Message
+
+from rasa.nlu.constants import TOKENS_NAMES, MESSAGE_ATTRIBUTES
+from underthesea import word_tokenize
+class VietnameseTokenizer(Tokenizer):
+
+    provides = [TOKENS_NAMES[attribute] for attribute in MESSAGE_ATTRIBUTES]
+
+    def __init__(self, component_config: Dict[Text, Any] = None) -> None:
+        super().__init__(component_config)
+    
+    def tokenize(self, message: Message, attribute: Text) -> List[Token]:
+        text = message.get(attribute)
+        words = word_tokenize(text)
+
+        return self._convert_words_to_tokens(words, text)
